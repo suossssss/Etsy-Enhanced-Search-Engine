15f9f3add78c6edd570351620d43ffe910c4fb3e
suosssss <lis86@pitt.edu>
2016-12-19 19:00:10 -0500

    First commit

---
+++ src/Classes/Path.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Path {	
+//	public static String indexSearchResult="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data/index_search_result";	
+//	public static String StopwordDir="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/stopword.txt";//address of stopwords.txt
+//	public static String resultPath="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data/abde.txt";
+//	public static String processedPath="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data/processed_search_result.txt";
+//    public static String TopicDir="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data/topics.txt";
+//    public static String Top10="/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data/top10.txt";
+	public static String indexSearchResult="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//data//index_search_result";	
+	public static String StopwordDir="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//stopword.txt";////address of stopwords.txt
+	public static String resultPath="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//data//abde.txt";
+	public static String processedPath="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//data//processed_search_result.txt";
+    public static String TopicDir="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//data//topics.txt";
+    public static String Top10="//Users//suoliyun//Documents//workspace//IR_finalproject//WebContent//WEB-INF//data//top10.txt";
+    public static String dataPath = "/Users/suoliyun/Documents/workspace/IR_finalproject/WebContent/WEB-INF/data";
+}
